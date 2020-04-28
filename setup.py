b1d4bf39ed77dd0279df884f699f8c91ba556a5a
Pyux <maciek.tomaszek@gmail.com>
2020-04-29 01:38:41 +0200

    - preparing pip release ready-version

---
+++ setup.py
@@ -0,0 +1,43 @@
+from distutils.core import setup
+
+setup(
+    name='postgres_restorer',
+    packages=['postgres_restorer'],
+    version='1.0',
+    license='MIT',
+    description='Simple, lightweight tool that manages postgres databases during integration tests.',
+    author='pyux',
+    url='https://github.com/pyux/postgres_restorer',
+    download_url='https://github.com/pyux/postgres_restorer/archive/1.0.tar.gz',
+    keywords=['postgres', 'integration', 'tests', 'restoring', 'database'],
+    install_requires=[
+        'attrs',
+        'certifi',
+        'chardet',
+        'codecov',
+        'coverage',
+        'idna',
+        'more-itertools',
+        'packaging',
+        'pluggy',
+        'psycopg2',
+        'py',
+        'pyparsing',
+        'pytest',
+        'pytest-cov',
+        'requests',
+        'six',
+        'urllib3',
+        'wcwidth'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.8.2',
+    ],
+)
